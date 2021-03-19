fb28c9cbd24680c044567d30124f69e731b2ea37
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-03-19 19:09:01 -0400

    completed the user endpoint & broke up the username

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
-const helpers = require('./utils/helpers');
+//const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const hbs = exphbs.create({ helpers });
+//const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,12 +25,13 @@ const sess = {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
 
 //===== HANDLEBARS TEMPLATE ENGINE =====//
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+//app.engine('handlebars', hbs.engine);
+//app.set('view engine', 'handlebars');
 
 //===== TURN ON ROUTES =====//
 app.use(routes);
