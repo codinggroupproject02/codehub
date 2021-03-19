d00098923eebe4ee897aab0d49973c516592402a
Johann Taylor <j.taylor1343@gmail.com>
2021-03-19 01:33:27 -0400

    Server setup

---
+++ server.js
@@ -0,0 +1,41 @@
+//======= DEPENDENCIES ========//
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const hbs = exphbs.create({ helpers });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+//===== HANDLEBARS TEMPLATE ENGINE =====//
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//===== TURN ON ROUTES =====//
+app.use(routes);
+
+//===== TURN ON CONNECTION TO DB AND SERVER =====//
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`NOW LISTENING ON PORT ${PORT}`));
+})
