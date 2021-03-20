a1e6b7b9d5bf6f700aaaeb13eaf56d6d52f49994
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-03-20 00:35:29 -0400

    Post findOne query complete

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -17,4 +17,30 @@ router.get('/', (req,res) => {
     });
 });
 
+router.get('/:id', (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'content', 'user_id'],
+        include: [
+            {
+                model: User,
+                attributes: ['first_name', 'last_name']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
