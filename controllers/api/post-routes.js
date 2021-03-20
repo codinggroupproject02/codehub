997482c748b81818ae1989751646d611be6c3fb0
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-03-20 00:25:39 -0400

    git find all and seed for post is complete

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
 const { Post, User } = require('../../models');
 
+router.get('/', (req,res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'content', 'user_id'],
+        include: [
+            {
+                model: User,
+                attributes: ['first_name', 'last_name']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
