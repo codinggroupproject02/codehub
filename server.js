750f455913a341b8d70858903bb398732d2dcb81
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-03-20 18:13:31 -0400

    connecting the homepage to handlebars

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const path = require('path');
+const path = require('path');
 // const session = require('express-session');
 // const helpers = require('./utils/helpers');
-// const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const hbs = exphbs.create({ helpers });
+//const hbs = exphbs.create({ helpers });    <-- Later
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,12 +26,13 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'public')));
 // app.use(session(sess));
 
 //===== HANDLEBARS TEMPLATE ENGINE =====//
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //===== TURN ON ROUTES =====//
 app.use(routes);
