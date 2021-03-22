238a66c991ed88742c99079e764ed1c1a3d02810
Johann Taylor <j.taylor1343@gmail.com>
2021-03-21 21:43:50 -0400

    Added user profiles

---
+++ controllers/profile-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { User } = require('../models')
+
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: [
+            'id',
+            'first_name',
+            'last_name',
+            'knowledgeable_in'
+        ]
+    })
+    .then(dbUserData => {
+        const users = dbUserData.map(user => user.get({ plain: true }));
+        res.render('profile-cards', { users });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
