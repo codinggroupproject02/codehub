092d96f7c7d032a9279a1023472d34ea7ed914d5
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-03-22 13:31:02 -0400

    handlebar helpers added

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-//const hbs = exphbs.create({ helpers });    <-- Later
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });   
 
 const app = express();
 const PORT = process.env.PORT || 3001;
